d505ad5fb3232390a59e72fefd45a2a1d1a9061d
nwgu <nwgu99@gmail.com>
2025-04-26 21:56:39 +0900

    실제 투두 데이터를 저장하기 위한 ArrayList 및 Todo 클래스(vo) 추가

--- src/todo/service/TodoService.java
+++ src/todo/service/TodoService.java
@@ -1,14 +1,21 @@
 package todo.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import todo.vo.Todo;
 
 /*
  * 실제 todo 기능 로직들을 담당하는 클래스
  * */
 public class TodoService {
 
+	// todo 데이터를 저장하는 저장소
+	List<Todo> todoList = new ArrayList<>();
+
 	public void postTodo(Scanner sc) {
-		
+
 	}
 
 }
---
+++ src/todo/vo/Todo.java
@@ -0,0 +1,45 @@
+package todo.vo;
+
+/*
+ * todo 속성을 가진 클래스
+ * */
+public class Todo {
+
+	private int todoId; // todo 고유 번호
+	private String title; // todo 제목
+	private String createAt; // 생성된 날짜
+	private boolean isCompleted; // 완료 여부
+
+	public int getTodoId() {
+		return todoId;
+	}
+
+	public void setTodoId(int todoId) {
+		this.todoId = todoId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(String createAt) {
+		this.createAt = createAt;
+	}
+
+	public boolean getIsCompleted() {
+		return isCompleted;
+	}
+
+	public void setIsCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+
+}
