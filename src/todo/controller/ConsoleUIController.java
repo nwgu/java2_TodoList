2c4b16b94675fab2417e164c33d25a9b2569aa5e
nwgu <nwgu99@gmail.com>
2025-04-26 21:35:16 +0900

    사용자가 직접적으로 상호작용하기 위한 ConsoleUIController 클래스 추가

---
+++ src/todo/controller/ConsoleUIController.java
@@ -0,0 +1,8 @@
+package todo.controller;
+
+/*
+ * 사용자 콘솔 UI 를 담당하는 컨트롤러
+ * */
+public class ConsoleUIController {
+
+}
--- src/todo/controller/SystemManager.java
+++ src/todo/controller/SystemManager.java
@@ -1,8 +1,17 @@
 package todo.controller;
 
+/*
+ * 투 두 리스트 프로그램의 전반적인 흐름을 담당하는 클래스
+ * */
 public class SystemManager {
 
+	private ConsoleUIController consoleUIController;
+
+	public SystemManager() {
+		this.consoleUIController = new ConsoleUIController();
+	}
+
 	public void start() {
-		
+
 	}
 }
