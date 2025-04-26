22cd92df9e82b9b266f4fbab4da0a88656bee545
nwgu <nwgu99@gmail.com>
2025-04-26 21:50:51 +0900

    controller 는 단순 요청만을 처리하는 클래스이다. 실제 todo 서비스를 구현하기 위해 TodoService 클래스 추가

--- src/todo/controller/ConsoleUIController.java
+++ src/todo/controller/ConsoleUIController.java
@@ -10,11 +10,11 @@ public class ConsoleUIController {
 	private TodoController todoController;
 	private boolean running = true;
 	Scanner sc = new Scanner(System.in);
-	
+
 	// TodoController todoController <-- 여기 있는 매개변수는 현재 실제 객체 값이 들어가있음.
 	// SystemManager 클래스에서 new 를 통해 저장된 변수를 전달 했기 때문에,
-	// TodoController 클래스에 작성된 메서드는 언제든 사용 가능 
-	public ConsoleUIController(TodoController todoController) {  
+	// TodoController 클래스에 작성된 메서드는 언제든 사용 가능
+	public ConsoleUIController(TodoController todoController) {
 		this.todoController = todoController;
 	}
 
@@ -31,7 +31,9 @@ public class ConsoleUIController {
 			System.out.println("프로그램 종료");
 			running = false;
 			sc.close();
-		} else {
+		}
+
+		else {
 			System.out.println("존재하지 않는 명령어");
 		}
 	}
--- src/todo/controller/TodoController.java
+++ src/todo/controller/TodoController.java
@@ -1,5 +1,13 @@
 package todo.controller;
 
+import todo.service.TodoService;
+
 public class TodoController {
 
+	private TodoService todoService;
+	
+	public TodoController() {
+		this.todoService = new TodoService();
+	}
+	
 }
